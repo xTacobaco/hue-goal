ef25dea1965931029cb007ceb3e92c0e15d0b98c
William Beinö <william.beino@hotmail.com>
2021-03-24 00:00:49 +0100

    added google analytics.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app'
+import "firebase/analytics";
 
 firebase.initializeApp({
     apiKey: 'AIzaSyB2T3s6Y5i6XAYK42mG1CwkrIASVdrHQGQ',
@@ -8,5 +9,7 @@ firebase.initializeApp({
     messagingSenderId: '382306850833',
     appId: '1:382306850833:web:620784da18ddc16e1379b0'
 })
+
+firebase.analytics();
 
 export default firebase
