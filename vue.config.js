8b7981f02e9d9bf4eeb70ff95ca7ca3b23bfa4ad
William Beinö <william.beino@hotmail.com>
2021-04-05 17:04:45 +0200

    smol seo fixes.

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
       config
       .plugin('html')
       .tap(args => {
-        args[0].title = 'Hue Goal'
+        args[0].title = 'Hue Goal - Tracking Tasks Made Colorful'
         return args
       })
     }
